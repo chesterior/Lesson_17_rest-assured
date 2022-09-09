e17b0516a12cc8fbec4205ad6e2c78df56e8ff77
Aleksandr Safronov <asafronov@sberhealth.ru>
2022-09-10 00:28:24 +0300

    api

--- src/test/java/tests/SelenoidTests.java
+++ src/test/java/tests/SelenoidTests.java
@@ -1,4 +1,22 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
 public class SelenoidTests {
+
+    // make request to https://selenoid.autotests.cloud/status
+    // total is 20
+
+    @Test
+    void checkTotal() {
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
 }
